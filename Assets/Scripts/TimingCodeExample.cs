633439bce45fda3e53ccf3accb24ef0429fa9be0
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-03-28 19:02:58 +0300

    update timing

--- Assets/Scripts/TimingCodeExample.cs
+++ Assets/Scripts/TimingCodeExample.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TimingCodeExample : MonoBehaviour
 {
-    public int cubeSpeed;
+    //public int cubeSpeed;
+    //public float timer;
+
+    private void Start()
+    {
+        Invoke("ShowMessage", 3); // invokeda fonksyionu string deðerde çaðýrabilriz
+    }
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime *cubeSpeed);
-        Debug.Log("Time : " + Time.time);
+        //transform.Translate(Vector3.forward * Time.deltaTime *cubeSpeed);
+        //Debug.Log("Time : " + Time.time);
+        //timer += Time.deltaTime;
+        //if(timer >= 2) 
+        //{
+        //    Debug.Log("2 saniye geçti");
+        //    timer = 0;
+        //}
+    }
+
+    private void ShowMessage()
+    {
+        Debug.Log("3 saniye sonra çalýþtý");
     }
 }
