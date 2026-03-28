e45ff9270ef91c8ec1fedc2bb86acfbc60fb3db1
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-03-28 18:56:04 +0300

    Update timingcode

--- Assets/Scripts/TimingCodeExample.cs
+++ Assets/Scripts/TimingCodeExample.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class TimingCodeExample : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int cubeSpeed;
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime *cubeSpeed);
+        Debug.Log("Time : " + Time.time);
     }
 }
